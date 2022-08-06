aadcdd46a635758bf47e0fc3d944f90690bbcd00
SviatLight <alex0fasten@gmail.com>
2022-08-06 11:09:08 +0300

    add react-router-dom, react-bootstrap

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
+import { Routes, Route } from "react-router-dom";
 import style from './App.module.css';
+import Landing from './components/landing/Landing';
 import UserInfo from './components/measurements/UserInfo';
 
 const App = () => {
   return (
     <div className={style.wrapper}>
-      <UserInfo />
+      <Routes>
+        <Route path="/" element={<Landing />} />
+        <Route path="/registration/weight" element={<UserInfo />} />
+      </Routes>
     </div>
   );
 }
