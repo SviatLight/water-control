59052ae03687153321b8fa4dee33039df29e3ccd
Sloian <andriysolonynka10@gmail.com>
2022-08-24 20:13:19 +0300

    WaterControlApp_update_1.0

--- src/components/app/WaterControl.jsx
+++ src/components/app/WaterControl.jsx
@@ -1,50 +1,53 @@
-import { useState,useRef } from "react";
-// import { Prev } from "react-bootstrap/esm/PageItem";
+import { useState, useRef } from "react";
 import style from "./WaterControl.module.css"
+// import { Prev } from "react-bootstrap/esm/PageItem";
 
 
 const WaterControl = () => {
 
   const [dailyRate, setDailyRate] = useState(1500);
-  let [consumed, setConsumed] = useState(0);
+  const [consumed, setConsumed] = useState(0);
   const [glass, setGlass] = useState(300);
-  let nubmerGlassess = Math.round(glass / dailyRate * 100);
-  let [nubmerGlasses, setNubmerGlasses] = useState(nubmerGlassess);
+  const nubmerGlassess = Math.round(glass / dailyRate * 100);
+  const [nubmerGlasses, setNubmerGlasses] = useState(nubmerGlassess);
 
 
   const percentage = useRef();
   const remaineds = useRef();
 
-
   const buttonClick = () =>  {
-    console.log(nubmerGlasses);
-    const percentageStyles = percentage.current.style;
-    const remainedsStyles = remaineds.current.style;
-    setNubmerGlasses(nubmerGlassess + nubmerGlasses)
+    if(consumed < dailyRate ){
+      // console.log(nubmerGlasses);
+      const percentageStyles = percentage.current.style;
+      const remainedsStyles = remaineds.current.style;
+      setNubmerGlasses(nubmerGlassess + nubmerGlasses)
 
-    if (nubmerGlasses === 100 || nubmerGlasses > 100){
-      remainedsStyles.height = 0;
-      remainedsStyles.visibility = "hidden";
-      percentageStyles.height = 400 * nubmerGlasses / 100  + "px";
-      setConsumed(consumed + glass);
-      alert('Ви випили денну норму');
-    } else{
-      console.log(nubmerGlasses)
-      percentageStyles.height = 400 * nubmerGlasses / 100  + "px";
-      console.log(percentageStyles.height)
-      percentageStyles.visibility = 'visible';
-      setConsumed(consumed + glass);
+      if (nubmerGlasses === 100 || nubmerGlasses > 100){
+        remainedsStyles.height = 0;
+        remainedsStyles.visibility = "hidden";
+        percentageStyles.height = 400 * nubmerGlasses / 100  + "px";
+        setConsumed(consumed + glass);
+      } else{
+        // console.log(nubmerGlasses)
+        percentageStyles.height = 400 * nubmerGlasses / 100  + "px";
+        // console.log(percentageStyles.height)
+        percentageStyles.visibility = 'visible';
+        setConsumed(consumed + glass);
+      }
     }
+
+    else {
+      return 0;
+    }
+
   };
 
   return (
     <div className= {style.wrapper}>
       {/* <div className={style.text_header_target}>{dailyRate} мл</div> */}
-      <div className={style.wrapper_target}>
-        <div id="watter_drunk" className={style.watter_drunk}>{consumed}</div>
-        <div className={style.target_dash}>/</div>
-        <div id="target" className={style.target}>{dailyRate}</div>
-      </div>
+      {consumed < dailyRate ? <div className={style.wrapper_target}>
+        <div id="watter_drunk" className={style.watter_drunk}>{consumed} / {dailyRate}</div>
+      </div>:<div className = {style.target_done} >Ви досягнули денну норму</div>}
 
       <div className={style.cup}>
         <div className={style.remained} id="remained"  ref={remaineds} >
@@ -52,9 +55,15 @@ const WaterControl = () => {
           <small>Remained</small>
         </div>
         <div className={style.percentage} ref={percentage} id="percentage" >{nubmerGlasses - nubmerGlassess}%</div>
-      </div>
 
-      <button className={style.button} onClick={() => buttonClick()} >Додати</button>
+      </div>
+      <label className={style.button}  onClick={() => buttonClick()} />
+
+{/*
+      <button type="submit" className={style.button} onClick={() => buttonClick()} >
+        <span className={style.span}></span>
+        <div className={style.liquid}></div>
+      </button> */}
 
     </div>
    );
