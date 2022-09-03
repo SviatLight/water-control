2486802191936f94026f4b7eb58f886a57c7055a
Sloian <andriysolonynka10@gmail.com>
2022-09-03 22:54:48 +0300

    Footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,15 +1,20 @@
 import React from "react";
 import style from "./Footer.module.css";
 
-
 const Footer = () => {
-
   return (
     <div className={style.wrapper}>
       <div className={style.wrapperSections}>
         <div className={`${style.section} ${style.sectionAbout}`}>
-          <h3 className={`${style.sectionHeader} ${style.sectionHeaderAbout}`}>About WatterControl</h3>
-          <p className={style.AboutDescription}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium sunt magnam ex neque, dignissimos labore, dolorem iusto ut at porro, adipisci alias provident? Distinctio fuga odio at, atque necessitatibus eos?</p>
+          <h3 className={`${style.sectionHeader} ${style.sectionHeaderAbout}`}>
+            About WatterControl
+          </h3>
+          <p className={style.AboutDescription}>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium
+            sunt magnam ex neque, dignissimos labore, dolorem iusto ut at porro,
+            adipisci alias provident? Distinctio fuga odio at, atque
+            necessitatibus eos?
+          </p>
           <ul className={style.wrapperSocial}>
             <li>
               <a href="/">
@@ -35,7 +40,11 @@ const Footer = () => {
         </div>
 
         <div className={`${style.section} ${style.sectionNavigation}`}>
-          <h3 className={`${style.sectionHeader} ${style.sectionHeaderNavigation}`}>Navigation</h3>
+          <h3
+            className={`${style.sectionHeader} ${style.sectionHeaderNavigation}`}
+          >
+            Navigation
+          </h3>
           <ul>
             <li className={style.listItem}>
               <a href="/">Pricing</a>
@@ -53,7 +62,11 @@ const Footer = () => {
         </div>
 
         <div className={`${style.section} ${style.sectionServices}`}>
-          <h3 className={`${style.sectionHeader} ${style.sectionHeaderServices}`}>Services</h3>
+          <h3
+            className={`${style.sectionHeader} ${style.sectionHeaderServices}`}
+          >
+            Services
+          </h3>
           <ul>
             <li className={style.listItem}>
               <a href="/">Team</a>
@@ -71,7 +84,11 @@ const Footer = () => {
         </div>
 
         <div className={`${style.section} ${style.sectionDownloads}`}>
-          <h3 className={`${style.sectionHeader} ${style.sectionHeaderDownloads}`}>Downloads</h3>
+          <h3
+            className={`${style.sectionHeader} ${style.sectionHeaderDownloads}`}
+          >
+            Downloads
+          </h3>
           <ul>
             <li className={style.listItem}>
               <a href="/">Get from the App Store</a>
@@ -83,11 +100,11 @@ const Footer = () => {
         </div>
       </div>
 
-      <div className={style.footerDescription}>Copyright Softland. All Rights Reversed <br />
-        Designed by <span>WatterBallanceTeam</span></div>
-
+      <div className={style.footerDescription}>
+        Copyright Softland. All Rights Reversed <br />
+        Designed by <span>WatterBallanceTeam</span>
+      </div>
     </div>
-
   );
 };
 
