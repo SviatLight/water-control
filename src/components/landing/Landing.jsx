b12f551df6e7d157020ea631fcdd394d87567be0
Anastasiya.Honcharyk <89775193+Anastasiyaaaaaa@users.noreply.github.com>
2022-09-11 18:34:03 +0300

    add header

--- src/components/landing/Landing.jsx
+++ src/components/landing/Landing.jsx
@@ -1,64 +1,18 @@
 import style from "./Landing.module.css";
-import background from "../../images/landingBackground.jpg";
 import React from "react";
 import { useNavigate, useOutletContext } from "react-router-dom";
-import Step from "../StepToSuccess/Step";
-import { Person } from "react-bootstrap-icons";
 import StepsToSuccess from "../StepToSuccess/StepsToSuccess";
 import RegistrationSteps from "../RegistrationSteps/RegistrationSteps";
 import Footer from "../Footer/Footer";
+import Header from "../Header/Header";
 
 const Landing = () => {
-  const navigate = useNavigate();
-  const { user, userHasSettings } = useOutletContext();
-
   return (
-    <div className={style.header}>
-      <div className={style.headerContent}>
-        <section className={style.headerContainer}>
-          <h2>Water</h2>
-          <p>
-            “Drinking water is like washing out your insides. The water will
-            cleanse the system, fill you up, decrease your caloric load and
-            improve the function of all your tissues.”
-          </p>
-
-          {user ? (
-            userHasSettings ? (
-              <button
-                className="btn btn-primary"
-                onClick={() => navigate("/app")}
-              >
-                Start
-              </button>
-            ) : (
-              <button
-                className="btn btn-primary"
-                onClick={() => navigate("/setup/gender")}
-              >
-                Start settings
-              </button>
-            )
-          ) : (
-            <button
-              className="btn btn-success"
-              onClick={() => navigate("/login")}
-            >
-              Sign in
-            </button>
-          )}
-        </section>
-        <div className={style.headerImg}>
-          <img
-            src={background}
-            className={style.backgroundImgLanding}
-            alt="background"
-          />
-        </div>
-      </div>
+    <div className={style.wrapper}>
+      <Header />
       <StepsToSuccess />
-      <RegistrationSteps/>
-      <Footer/>
+      <RegistrationSteps />
+      <Footer />
     </div>
   );
 };
